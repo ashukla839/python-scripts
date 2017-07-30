36162ff44aaecb1f71e3a7d77a91b46c6584bdf2
Abul Basar <abulbasar@users.noreply.github.com>
2017-07-30 22:23:55 +0530

    Update stream-file-over-stocket.py

--- stream-file-over-stocket.py
+++ stream-file-over-stocket.py
@@ -2,6 +2,7 @@ import time
 import socket
 
 if __name__ == "__main__":
+    filename = input("Enter the file to stream: ")
     host, port = "0.0.0.0", 9999
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind((host,port))
@@ -11,7 +12,7 @@ if __name__ == "__main__":
     (client, address) = serversocket.accept()
     print("Connection from", address)
     count = 0
-    for line in open(input("Enter the file to stream: ")):
+    for line in open(filename):
         print("Row number", count)
         count += 1
         client.send(line)
